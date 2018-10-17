8ee2abbc4a48267be20a6ebd3029bf80520370fa
Ymasri93 <yasir.masri@gmail.com>
2018-10-17 17:27:04 +0300

    removed unnecessary styles and removed the text-alignment for the pinned column

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ const centerKey = 'text-align';
 objCENTER[centerKey] = 'center';
 key = 'text-align';
 objCENTER[key] = 'center';
-let objBorderLeft = { 'box-shadow': '-.5px 0 0 #000' };
-let objBorderRight = { 'box-shadow': '-1.2px 0 0 #000' };
 let cellStyle = {};
-cellStyle = Object.assign(cellStyle, objCENTER);
-cellStyle = Object.assign(cellStyle, objBorderLeft, objBorderRight);
+// cellStyle = {...cellStyle, width: '400px'}
 // By creating the template on our own we can add the arrows (for the years) and change the text here / TESTING PHASE
 let columnDefs = [{
-    headerName: 'Year', field: 'yearAndName', pinned: 'left', headerComponentParams: {
+    headerName: 'Year', field: 'yearAndName', pinned: 'left', width: 400, headerComponentParams: {
         template:
             `<div class="ag-cell-label-container" role="presentation"> 
             <span ref="eText" class="ag-header-cell-text" role="columnheader"></span> 
@@ -26,15 +23,8 @@ for (const year of years) {
         objCENTER[centerKey] = 'center';
         key = 'text-align';
         objCENTER[key] = 'center';
-        let objBorderLeft = { 'box-shadow': '-.5px 0 0 #000' };
-        let objBorderRight = { 'box-shadow': '-1.2px 0 0 #000' };
-        if (month === 'jan') {
-            objBorderLeft = { 'box-shadow': '0px solid #000' };
-            objBorderRight = { 'box-shadow': '-1.2px 0 0 #000' };
-        }
         let cellStyle = {};
         cellStyle = Object.assign(cellStyle, objCENTER);
-        cellStyle = Object.assign(cellStyle, objBorderLeft, objBorderRight);
         // By creating the template on our own we can add the styling we want to the active draft month header / TESTING PHASE
         columnDefs.push({
             headerName: month.toUpperCase(), field: month + year, cellStyle, headerComponentParams: {
